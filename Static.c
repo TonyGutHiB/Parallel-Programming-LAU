ab3e01e4006fb6e366d42a82c1b021a1c7d35acb
TonyGutHiB <160217865+TonyGutHiB@users.noreply.github.com>
2024-02-17 13:21:27 +0200

    Static.c

--- Static.c
+++ Static.c
@@ -1,87 +1,89 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 #include <mpi.h>
 
 #define WIDTH 640
 #define HEIGHT 480
+#define MAX_ITER 255
 
-int main(int argc, char** argv) {
-    double commtime;
-    MPI_Init(&argc, &argv);
-    double start = MPI_Wtime();
+struct complex {
+    double real;
+    double imag;
+};
+
+int cal_pixel(struct complex c) {
+    double z_real = 0;
+    double z_imag = 0;
+    double z_real2, z_imag2, lengthsq;
+    int iter = 0;
+    do {
+        z_real2 = z_real * z_real;
+        z_imag2 = z_imag * z_imag;
+        z_imag = 2 * z_real * z_imag + c.imag;
+        z_real = z_real2 - z_imag2 + c.real;
+        lengthsq = z_real2 + z_imag2;
+        iter++;
+    } while ((iter < MAX_ITER) && (lengthsq < 4.0));
+    return iter;
+}
+
+void save_pgm(const char *filename, int image[HEIGHT][WIDTH]) {
+    FILE *pgmimg;
+    int temp;
+    pgmimg = fopen(filename, "wb");
+    fprintf(pgmimg, "P2\n");
+    fprintf(pgmimg, "%d %d\n", WIDTH, HEIGHT);
+    fprintf(pgmimg, "255\n");
+    int count = 0;
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            temp = image[i][j];
+            fprintf(pgmimg, "%d ", temp);
+        }
+        fprintf(pgmimg, "\n");
+    }
+    fclose(pgmimg);
+}
+
+int main(int argc, char *argv[]) {
+    int image[HEIGHT][WIDTH];
+    double AVG = 0;
+    int N = 10;
+    double total_time[N];
+    struct complex c;
+
     int rank, size;
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    double real_min = -1.0;
-    double real_max = 1.0;
-    double imag_min = -1.0;
-    double imag_max = 1.0;
-    int max_counter = 1000;
-    int sub_region = HEIGHT / size;
-    int start_row = sub_region * rank;
-    int end_row = start_row + sub_region;
-    if (rank == size - 1) {
-        end_row = HEIGHT;
-    }
 
-    int* row = (int*)malloc(sizeof(int) * WIDTH);
-    int* data = (int*)malloc(sizeof(int) * WIDTH * sub_region);
+    int chunk_size = HEIGHT / size;
+    int start_row = rank * chunk_size;
+    int end_row = (rank == size - 1) ? HEIGHT : (rank + 1) * chunk_size;
 
-    for (int y = start_row; y < end_row; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            double c_real = real_min + (real_max - real_min) * x / WIDTH;
-            double c_imag = imag_min + (imag_max - imag_min) * y / HEIGHT;
-            double z_real = 0.0;
-            double z_imag = 0.0;
+    for (int k = 0; k < N; k++) {
+        clock_t start_time = clock();
 
-            int counter = 0;
-            while (z_real * z_real + z_imag * z_imag < 4.0 && counter < max_counter) {
-                double next_z_real = z_real * z_real - z_imag * z_imag + c_real;
-                double next_z_imag = 2.0 * z_real * z_imag + c_imag;
-                z_real = next_z_real;
-                z_imag = next_z_imag;
-                counter++;
-            }
-
-            if (counter == max_counter) {
-                row[x] = 0;
-            } else {
-                row[x] = counter % 256;
+        for (int i = start_row; i < end_row; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                c.real = (j - WIDTH / 2.0) * 4.0 / WIDTH;
+                c.imag = (i - HEIGHT / 2.0) * 4.0 / HEIGHT;
+                image[i][j] = cal_pixel(c);
             }
         }
-        int r_index = (y - start_row) * WIDTH;
 
-        for (int x = 0; x < WIDTH; x++) {
-            data[r_index + x] = row[x];
-        }
+        clock_t end_time = clock();
+
+        total_time[k] = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+        printf("Execution time of trial [%d]: %f seconds\n", k, total_time[k]);
+        AVG += total_time[k];
     }
 
-    free(row);
-    int* Data = NULL;
-    if (rank == 0) {
-        Data = (int*)malloc(sizeof(int) * WIDTH * HEIGHT);
-    }
-    
-    double starttime = MPI_Wtime();
-    MPI_Gather(data, WIDTH * sub_region, MPI_INT, Data, WIDTH * sub_region, MPI_INT, 0, MPI_COMM_WORLD);
-    double endtime = MPI_Wtime();
-    commtime = endtime - starttime;
-    free(data);
-
-    if (rank == 0) {
-        FILE* fp = fopen("mandelbrot.pgm", "wb");
-        fprintf(fp, "P5\n%d %d\n255\n", WIDTH, HEIGHT);
-        for (int i = 0; i < WIDTH * HEIGHT; i++) {
-            fputc(Data[i], fp);
-        }
-        fclose(fp);
-        free(Data);
-    }
-
-    double end = MPI_Wtime();
-    double elapsed = end - start;
-    printf("Elapsed time: %f seconds\n", elapsed);
-    printf("commtime: %f seconds\n", commtime);
     MPI_Finalize();
+
+    save_pgm("mandelbrot.pgm", image);
+    printf("The average execution time of %d trials is: %f ms\n", N, AVG / N * 1000);
+
     return 0;
 }
+
